c72153c8d2c2465e5e427b64b1951fe6ffe1407c
Christian Vajgel <christian.vajgel@al.infnet.edu.br>
2024-04-01 19:13:00 -0300

    Refatoracao limpeza Produtos

--- src/components/pages/Produtos.vue
+++ src/components/pages/Produtos.vue
@@ -14,8 +14,6 @@ onMounted(() => {
 
 function listarTodosOsProdutos(){
 
-  console.log("listar");
-
   axios.get('https://localhost:7173/api/Produto')
       .then(response => {
         data.value = response.data;
@@ -26,15 +24,7 @@ function listarTodosOsProdutos(){
       .finally(() => {
         loading.value = false;
       });
-
-  // console.log(data);
 }
-
-
-
-// const carrinho = ref({
-//   id: gerarUUID()
-// });
 
 const carrinho = ref({
   id: obterIdDoCarrinho()
@@ -43,7 +33,6 @@ const carrinho = ref({
 async function adicionarProdutoNoCarrinho(idProduto) {
 
   const jsonForm = JSON.stringify(carrinho.value);
-  console.log(jsonForm);
 
   try {
     const response = await axios.post('https://localhost:7173/api/Carrinho/adicionar-produto-carrinho', jsonForm, {
@@ -53,11 +42,7 @@ async function adicionarProdutoNoCarrinho(idProduto) {
         'IdProduto':idProduto
       }
     });
-
-    console.log(response.request);
-
     console.log('ADICIONAR CARRINHO -> Resposta do servidor:', response.data);
-
     await router.push("/produtos");
 
   } catch (error) {
@@ -71,54 +56,6 @@ async function adicionarProdutoNoCarrinho(idProduto) {
 
   <div>
 
-    <!--    Carrousel ########## -->
-<!--    <div>-->
-
-
-<!--      &lt;!&ndash; Slider &ndash;&gt;-->
-<!--      <div data-hs-carousel='{-->
-<!--          "loadingClasses": "opacity-0"-->
-<!--        }' class="relative">-->
-<!--        <div class="hs-carousel relative overflow-hidden w-full min-h-[350px] bg-white rounded-lg">-->
-<!--          <div class="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">-->
-<!--            <div class="hs-carousel-slide">-->
-<!--              <div class="flex justify-center h-full p-6">-->
-<!--                &lt;!&ndash;                <span class="self-center text-4xl transition duration-700">First slide</span>&ndash;&gt;-->
-<!--                <img src="https://images.unsplash.com/photo-1561986810-4f3ba2f46ceb?q=80&w=2070&auto=format&fit=crop" class="self-center transition duration-700" alt="cafe"/>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--            <div class="hs-carousel-slide">-->
-<!--              <div class="flex justify-center h-full p-6">-->
-<!--                <img src="https://images.unsplash.com/photo-1629248989876-07129a68946d?q=80&w=2069&auto=format&fit=crop" class="self-center transition duration-700" alt="cafe"/>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--            <div class="hs-carousel-slide">-->
-<!--              <div class="flex justify-center h-full p-6">-->
-<!--                <img src="https://images.unsplash.com/photo-1584850379382-0eac0eec7011?q=80&w=2092&auto=format&fit=crop" class="self-center transition duration-700" alt="cafe"/>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-
-<!--        <div class="hs-carousel-pagination flex justify-center absolute bottom-3 start-0 end-0 space-x-2">-->
-<!--          <span class="hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 w-3 h-3 border border-gray-400 rounded-full cursor-pointer"></span>-->
-<!--          <span class="hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 w-3 h-3 border border-gray-400 rounded-full cursor-pointer"></span>-->
-<!--          <span class="hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 w-3 h-3 border border-gray-400 rounded-full cursor-pointer"></span>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--      &lt;!&ndash; End Slider &ndash;&gt;-->
-
-<!--    </div>-->
-    <!--   End Carrousel ########## -->
-
-<!--    <br>-->
-<!--    <br>-->
-
-<!--    <div>-->
-<!--      <div style="text-align: center;" v-if="loading">Carregando...</div>-->
-<!--      <div style="text-align: center;" v-else>RESPONSE: <br> {{ data }}</div>-->
-<!--    </div>-->
-
     <br>
     <br>
     <br>
@@ -126,25 +63,13 @@ async function adicionarProdutoNoCarrinho(idProduto) {
     <div class="mt-5 max-w-2xl mx-auto text-center mb-10 lg:mb-14">
       <h2 class="text-2xl font-bold md:text-4xl md:leading-tight titulo-header">Produtos</h2>
     </div>
-
-    <!--    Card group ##########-->
-
     <div>
-
-      <!-- Card Blog -->
       <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-        <!-- Grid -->
         <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
-
-          <!-- Card -->
           <div v-for="(produto, index) in data" :key="produto.id"
                class="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-            <!--            <div class="h-52 flex flex-col justify-center items-center rounded-t-xl">-->
             <div class="flex flex-col justify-center items-center rounded-t-xl">
-
-              <img :src="produto.imagem"
-                   class="self-center imagem-card border rounded-xl mt-4 md:mt-6 shadow-sm group-hover:scale-105 transition-transform duration-500 ease-in-out" alt="cafe"/>
-
+              <img :src="produto.imagem" class="self-center imagem-card border rounded-xl mt-4 md:mt-6 shadow-sm group-hover:scale-105 transition-transform duration-500 ease-in-out" alt="cafe"/>
             </div>
             <div class="p-4 md:p-6">
               <span class="block mb-1 text-xs font-semibold uppercase text-yellow-800 dark:text-rose-500">
@@ -158,100 +83,17 @@ async function adicionarProdutoNoCarrinho(idProduto) {
               </p>
             </div>
             <div class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">
-
-<!--              <router-link :to="`/detalhar-produto/${produto.idProduto}`"-->
-              <router-link :to="`/detalhar-produto/${produto.id}`"
-                           class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-es-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-
+              <router-link :to="`/detalhar-produto/${produto.id}`" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-es-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none">
                 Conheça
-
               </router-link>
-
-              <a @click="adicionarProdutoNoCarrinho(produto.id)" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-ee-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                 href="#comprar">
+              <a @click="adicionarProdutoNoCarrinho(produto.id)" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-ee-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" href="#comprar">
                 Comprar
               </a>
-
             </div>
           </div>
-          <!-- End Card -->
-
-
-
-          <!--          &lt;!&ndash; Card &ndash;&gt;-->
-          <!--          <div class="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">-->
-          <!--            <div class="h-52 flex flex-col justify-center items-center rounded-t-xl">-->
-
-          <!--              <img src="https://images.unsplash.com/photo-1524350876685-274059332603?q=80&w=2071&auto=format&fit=crop" class="self-center" alt="cafe" width="75%" height="75%"/>-->
-
-          <!--            </div>-->
-          <!--            <div class="p-4 md:p-6">-->
-          <!--              <span class="block mb-1 text-xs font-semibold uppercase text-yellow-800 dark:text-blue-500">-->
-          <!--                Brasil-->
-          <!--              </span>-->
-          <!--              <h3 class="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">-->
-          <!--                Mogiana-->
-          <!--              </h3>-->
-          <!--              <p class="mt-3 text-gray-500">-->
-          <!--                Do jeito que o brasileiro gosta.-->
-          <!--              </p>-->
-          <!--            </div>-->
-          <!--            <div class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">-->
-          <!--              <a class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-es-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">-->
-          <!--                Conheça-->
-          <!--              </a>-->
-          <!--              <a class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-ee-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">-->
-          <!--                Comprar-->
-          <!--              </a>-->
-          <!--            </div>-->
-          <!--          </div>-->
-          <!--          &lt;!&ndash; End Card &ndash;&gt;-->
-
-          <!--          &lt;!&ndash; Card &ndash;&gt;-->
-          <!--          <div-->
-          <!--              class="group flex flex-col h-full bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">-->
-          <!--            <div class="h-52 flex flex-col justify-center items-center rounded-t-xl">-->
-
-          <!--              <img src="https://images.unsplash.com/photo-1511759066510-46958c3fffa0?q=80&w=2076&auto=format&fit=crop"-->
-          <!--                   class="self-center" alt="cafe" width="75%" height="75%"/>-->
-
-          <!--            </div>-->
-          <!--            <div class="p-4 md:p-6">-->
-          <!--              <span class="block mb-1 text-xs font-semibold uppercase text-yellow-800 dark:text-rose-500">-->
-          <!--                Jamaica-->
-          <!--              </span>-->
-          <!--              <h3 class="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">-->
-          <!--                Blue Mountain-->
-          <!--              </h3>-->
-          <!--              <p class="mt-3 text-gray-500">-->
-          <!--                Um café das alturas.-->
-          <!--              </p>-->
-          <!--            </div>-->
-          <!--            <div-->
-          <!--                class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700">-->
-          <!--              <a class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-es-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"-->
-          <!--                 href="#">-->
-          <!--                Conheça-->
-          <!--              </a>-->
-          <!--              <a class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-ee-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"-->
-          <!--                 href="#">-->
-          <!--                Comprar-->
-          <!--              </a>-->
-          <!--            </div>-->
-          <!--          </div>-->
-          <!--          &lt;!&ndash; End Card &ndash;&gt;-->
-
-
         </div>
-        <!-- End Grid -->
       </div>
-      <!-- End Card Blog -->
-
     </div>
-
-    <!--   End Card group ##########-->
-
-
   </div>
 
 </template>
